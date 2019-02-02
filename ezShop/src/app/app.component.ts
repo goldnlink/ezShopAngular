0e9ea29a5a74b17d30bff13948f7260f7e0205cb
Aurelien Le Gonidec <aurelien.legonidec@ebiznext.com>
2019-02-02 22:03:45 +0100

    Exemple avec compteur de binding (one way view=>controller, 1 way controller=>view, 2 way)

--- ezShop/src/app/app.component.ts
+++ ezShop/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'ezShop';
+  title = 'ezShop : le meilleur du shop';
+  message = 'Reprenez vos habitudes shopping en main';
+  image = 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png';
+  compteur = 0;
+
+  Reset() {
+    this.compteur = 0;
+    this.AfficherCompteur();
+  }
+
+  AjouterUn() {
+    this.compteur++;
+    this.AfficherCompteur();
+  }
+
+  AfficherCompteur() {
+    console.log(`Compteur a la valeur ${this.compteur}`);
+  }
 }
