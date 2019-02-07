4adbfafba1f736113822f8c6e1cb9a2a9e34c058
Aurelien Le Gonidec <aurelien.legonidec@ebiznext.com>
2019-02-07 05:39:09 +0100

    Exercice 8

--- ezShop/src/app/details-personne/details-personne.component.ts
+++ ezShop/src/app/details-personne/details-personne.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Personne } from '../personne';
-import { MatFormField, MatInput } from '@angular/material';
 
 @Component({
   selector: 'app-details-personne',
   templateUrl: './details-personne.component.html',
   styleUrls: ['./details-personne.component.less']
 })
-export class DetailsPersonneComponent implements OnInit {
+export class DetailsPersonneComponent {
   @Input() personne: Personne;
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }
