fbb8aafe206c5f3127bcbb720d28ebea7d4377ee
Aurelien Le Gonidec <aurelien.legonidec@ebiznext.com>
2019-02-18 22:32:53 +0100

    ondestroy et filtre sur best deals

--- ezShop/src/app/best-deals/best-deals.component.ts
+++ ezShop/src/app/best-deals/best-deals.component.ts
@@ -1,18 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Produit } from '../model/produit';
 import { ProduitsService } from '../services/produits.service';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-best-deals',
   templateUrl: './best-deals.component.html',
   styleUrls: ['./best-deals.component.less']
 })
-export class BestDealsComponent implements OnInit {
+export class BestDealsComponent implements OnInit, OnDestroy {
+
   listeProduits: Array<Produit> = new Array<Produit>();
-  constructor(private produitService: ProduitsService) { }
+  categoryId: number;
+  constructor(private produitService: ProduitsService, private categoryService: CategoryService) { }
 
   ngOnInit() {
-    this.produitService.getBestDeals().subscribe(result => this.listeProduits = result);
+    this.categoryService.selectedCategory.subscribe(categoryId => {
+      this.categoryId = categoryId;
+      this.refreshListe();
+    });
+    this.refreshListe();
+  }
+
+  ngOnDestroy() {
+    this.categoryService.resetSelectedCategory();
+  }
+
+  refreshListe() {
+    this.produitService.getBestDeals().subscribe(result => {
+      if (this.categoryId) {
+        this.listeProduits = result.filter(produit => produit.categoryId === this.categoryId);
+      } else {
+        this.listeProduits = result;
+      }
+    });
   }
 
   getDetails(produit: Produit) {
